513ee4cbf7597cb14a6796dec357af0148a19937
Waweru001 <wawerumwangi867@gmail.com>
2023-02-21 15:03:41 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *  list all the natural numbers below 10
+ * a program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line.
  *
  * Return: void
 */
